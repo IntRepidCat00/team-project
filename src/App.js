3b48b9585e466d31df09078d2ed33ed1a3f504b7
vadymprosvirov <vadym.prosvirov.pz.2021@lpnu.ua>
2023-12-13 23:01:00 +0200

    Vadym 1st commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import VadymImg from "./image/VadymImg.jpg";
 import "./App.css";
 
 function App() {
@@ -8,7 +9,14 @@ function App() {
       <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
         <div></div>
         <div></div>
-        <div></div>
+        <div>
+          <h3>Vadym Prosvirov</h3>
+          <p>Role: Developer</p>
+          <img
+            style={{ width: "100px", height: "100px" }}
+            src={VadymImg}
+          ></img>
+        </div>
         <div></div>
       </div>
     </div>
