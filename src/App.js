53940fb769b2af7d6e8de0a19d96f83c5dade70a
DESKTOP-THJ10I8\maksy <maksym.romaniuk.pz.2021@lpnu.ua>
2023-12-13 23:09:59 +0200

    Max third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from "./logo.svg";
+import logo from "./img/max.png";
 import "./App.css";
-
+ 
 function App() {
   return (
     <div className="App">
       <h3>Team Project</h3>
       <div style={{ display: "flex", flexDirection: "column", gap: "24px" }}>
         <div></div>
-        <div></div>
+        <div>
+          <h3>Max Romaniuk</h3>
+          <p>Role: UI Designer</p>
+          <img
+            style={{ width: "100px", height: "100px" }}
+            src={logo}
+          ></img>
+        </div>
         <div></div>
         <div></div>
       </div>
